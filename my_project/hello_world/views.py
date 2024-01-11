42988e5f6e9cba85861fc76cdc662d995ee46f61
Lydia Young <lydsjoyy@gmail.com>
2024-01-11 12:18:15 +0000

    commit all chages

--- my_project/hello_world/views.py
+++ my_project/hello_world/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # create your veiws here
 def index(request):
-    return HttpResponse("Hello, world!")
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
     
 
